31952d3a45701bddf62d47a94b83a89a2beac7f4
nayan-debug <ndeka3057@gmail.com>
2025-08-09 11:37:56 +0530

    Add files via upload

---
+++ Assignment-1b.py
@@ -0,0 +1,7 @@
+#String input for uname
+a=input("Enter your first name :")
+b=input("Enter your last name : ")
+#Concatinate first name and last name
+c=(a +" "+b)
+#message greetings
+print("Hello "+c+" ! Welcome to the Python program.")
---
+++ Assignmet-1a.py
@@ -0,0 +1,16 @@
+#Enter Numbers
+a=input("Enter the first number : ")
+b=input("Enter the second number : ")
+# Assign Float Variabls
+a=float(a)
+b=float(b)
+#Do the Calculation
+A=(a+b)
+S=(a-b)
+M=(a*b)
+D=(a/b)
+#Displays the Resule
+print("Addition       :",A)
+print("Subtraction    :",S)
+print("Multiplication :",M)
+print("Division       :",D)
